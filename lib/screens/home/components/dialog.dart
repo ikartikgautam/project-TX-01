f9657ec72c0d2e973a36b9dcd58dcbd9ff03776f
thiskartik <gautam.kartik05@gmail.com>
2020-07-04 08:19:33 +0530

    camera working with basic UI

--- lib/screens/home/components/dialog.dart
+++ lib/screens/home/components/dialog.dart
@@ -1,5 +1,5 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:musestar/screens/home/home.dart';
 import 'package:musestar/screens/home/menus/camera.dart';
 
 class BottomDialog extends StatefulWidget {
@@ -35,72 +35,74 @@ class _BottomDialogState extends State<BottomDialog> {
           borderRadius: BorderRadius.only(
               topLeft: Radius.circular(24), topRight: Radius.circular(24))),
       height: _height,
-      child: Column(
-        children: <Widget>[
-          SizedBox(height: 6),
-          IconButton(
-              padding: EdgeInsets.all(0),
-              icon: _modalIcon,
-              onPressed: () {
-                if (_state == false) {
-                  changeHeight();
-                } else {
-                  Navigator.pop(context);
-                }
-              }),
-          GestureDetector(
-            onTap: () {
-              print('Clicked');
-              changeHeight();
-            },
-            child: Container(
-              width: double.infinity,
-              height: 40,
-              child: Center(child: Text('What is in your mind ?')),
-              margin: EdgeInsets.fromLTRB(24, 0, 24, 12),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(24), color: Colors.white),
+      child: SafeArea(
+              child: Column(
+          children: <Widget>[
+            SizedBox(height: 6),
+            IconButton(
+                padding: EdgeInsets.all(0),
+                icon: _modalIcon,
+                onPressed: () {
+                  if (_state == false) {
+                    changeHeight();
+                  } else {
+                    Navigator.pop(context);
+                  }
+                }),
+            GestureDetector(
+              onTap: () {
+                print('Clicked');
+                changeHeight();
+              },
+              child: Container(
+                width: double.infinity,
+                height: 40,
+                child: Center(child: Text('What is in your mind ?')),
+                margin: EdgeInsets.fromLTRB(24, 0, 24, 12),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(24), color: Colors.white),
+              ),
             ),
-          ),
-          Container(
-              margin: EdgeInsets.fromLTRB(24, 6, 24, 0),
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                      child: Container(
-                          child: IconButton(
-                              icon: Icon(
-                                Icons.send,
-                                size: 40,
-                                color: Colors.white,
-                              ),
-                              onPressed: null))),
-                  Expanded(
-                      child: Container(
-                          child: IconButton(
-                              icon: Icon(
-                                Icons.camera,
-                                size: 50,
-                                color: Colors.white,
-                              ),
-                              onPressed: () {
-                                Navigator.push(
-                                    context,
-                                    new MaterialPageRoute(
-                                        builder: (context) => Camera()));
-                              }))),
-                  Expanded(
-                      child: Container(
-                          child: IconButton(
-                              icon: Icon(
-                                Icons.ac_unit,
-                                size: 40,
-                                color: Colors.white,
-                              ),
-                              onPressed: null))),
-                ],
-              ))
-        ],
+            Container(
+                margin: EdgeInsets.fromLTRB(24, 6, 24, 0),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                        child: Container(
+                            child: IconButton(
+                                icon: Icon(
+                                  Icons.send,
+                                  size: 40,
+                                  color: Colors.white,
+                                ),
+                                onPressed: null))),
+                    Expanded(
+                        child: Container(
+                            child: IconButton(
+                                icon: Icon(
+                                  Icons.camera,
+                                  size: 50,
+                                  color: Colors.white,
+                                ),
+                                onPressed: () {
+                                  Navigator.push(
+                                      context,
+                                      new CupertinoPageRoute(
+                                          builder: (context) => Camera()));
+                                }))),
+                    Expanded(
+                        child: Container(
+                            child: IconButton(
+                                icon: Icon(
+                                  Icons.ac_unit,
+                                  size: 40,
+                                  color: Colors.white,
+                                ),
+                                onPressed: null))),
+                  ],
+                ))
+          ],
+        ),
       ),
     );
   }
--- lib/screens/home/menus/camera.dart
+++ lib/screens/home/menus/camera.dart
@@ -1,13 +1,97 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
 
 class Camera extends StatefulWidget {
+  // Camera({key:key}):
   @override
   _CameraState createState() => _CameraState();
 }
 
 class _CameraState extends State<Camera> {
+  CameraController _controller;
+  Future<void> _controllerInializer;
+
+  Future<CameraDescription> getCamera() async {
+    final c = await availableCameras();
+    return c.first;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getCamera().then((camera) => {
+          setState(() {
+            _controller = CameraController(camera, ResolutionPreset.high);
+            _controllerInializer = _controller.initialize();
+          })
+        });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(child: Text('Camera'));
+    double deviceWidth = MediaQuery.of(context).size.width;
+    return FutureBuilder(
+      future: _controllerInializer,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return Scaffold(
+            body: SafeArea(
+                child: Stack(
+              children: <Widget>[
+                CameraPreview(_controller),
+                Positioned(
+                  child: IconButton(
+                      icon: Icon(Icons.arrow_back),
+                      color: Colors.white,
+                      onPressed: () {
+                        Navigator.pop(context);
+                      }),
+                  top: 0,
+                  left: 0,
+                ),
+                Positioned(
+                  child: IconButton(
+                      icon: Icon(Icons.camera_front),
+                      color: Colors.white,
+                      onPressed: () {}),
+                  top: 0,
+                  right: 0,
+                ),
+                Positioned(
+                  child: Container(
+                    width: deviceWidth,
+                    height: 100,
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: IconButton(
+                            icon: Icon(Icons.lens, size: 60),
+                            onPressed: () {},
+                            color: Colors.white,
+                          ),
+                        ),
+                        SizedBox(height: 12)
+                      ],
+                    ),
+                  ),
+                  bottom: 0,
+                )
+              ],
+            )),
+          );
+        } else {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
   }
 }
